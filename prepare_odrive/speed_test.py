c0349da32dc392108cc8425914df7c5fdcf0c817
Jev Kuznetsov <jev.kuznetsov@gmail.com>
2023-11-20 16:25:17 +0000

    add tools

--- prepare_odrive/speed_test.py
+++ prepare_odrive/speed_test.py
@@ -4,11 +4,22 @@
 
  Copyright (c) 2023 ROX Automation - Jev Kuznetsov
 """
-from time import sleep
+import time
 import odrive  # type: ignore
 import odrive.enums as enums  # type: ignore
 
+from odrive_can.tools import UDP_Client
+
+
+class OdriveError(Exception):
+    """custom exception"""
+
+
+# send data to plotjuggler
+udp = UDP_Client()
+
 drv = odrive.find_any()
+
 
 print(f"Found ODrive {drv.serial_number}")
 
@@ -17,27 +28,22 @@ drv.axis0.requested_state = enums.AxisState.CLOSED_LOOP_CONTROL
 drv.axis0.config.enable_watchdog = False
 
 
-def is_error(drv):
+def check_error(drv):
     """check if there are any errors"""
-    ax = drv.axis0
 
-    for var in [
-        ax.error,
-        ax.motor.error,
-        ax.encoder.error,
-        ax.controller.error,
-        ax.sensorless_estimator.error,
-    ]:
+    names = [
+        "error",
+        "motor.error",
+        "encoder.error",
+        "controller.error",
+        "sensorless_estimator.error",
+    ]
+
+    for name in names:
+        var = getattr(drv.axis0, name)
         if var > 0:
-            return True
+            raise OdriveError(f"{name} : {var} ")
 
-    return False
-
-
-# check for errors
-if is_error(drv):
-    print("ERROR found, exiting")
-    exit(1)
 
 # set positiion to zero
 drv.axis0.encoder.set_linear_count(0)
@@ -49,14 +55,26 @@ setpoints = list(range(0, 50)) + 20 * [50] + list(range(50, 0, -1))
 def follow_curve(setpoints, delay=0.1):
     """follow curve consisting of velocity setpoints"""
 
+    # check for errors
+    check_error(drv)
+
     for setpoint in setpoints:
-        print(f"Setting setpoint to {setpoint}")
+        print(f"{setpoint=}")
         drv.axis0.controller.input_vel = setpoint
-        sleep(delay)
-        # print current velocity
-        print(f"Current velocity: {drv.axis0.encoder.vel_estimate}")
-        # print current position
-        print(f"Current position: {drv.axis0.encoder.pos_estimate}")
+
+        data = {
+            "ts": time.time(),
+            "sp": setpoint,
+            "vel": drv.axis0.encoder.vel_estimate,
+            "pos": drv.axis0.encoder.pos_estimate,
+        }
+
+        udp.send(data)
+        time.sleep(delay)
 
 
-follow_curve(setpoints)
+try:
+    while True:
+        follow_curve(setpoints)
+except KeyboardInterrupt:
+    print("interrupted")
