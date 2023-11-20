707f05a7b72e16078c69a92b045a821ff4c0d6a6
Jev Kuznetsov <jev.kuznetsov@gmail.com>
2023-11-20 14:44:13 +0000

    refactored speed test

--- prepare_odrive/speed_test.py
+++ prepare_odrive/speed_test.py
@@ -5,8 +5,8 @@
  Copyright (c) 2023 ROX Automation - Jev Kuznetsov
 """
 from time import sleep
-import odrive
-import odrive.enums as enums
+import odrive  # type: ignore
+import odrive.enums as enums  # type: ignore
 
 drv = odrive.find_any()
 
@@ -17,18 +17,46 @@ drv.axis0.requested_state = enums.AxisState.CLOSED_LOOP_CONTROL
 drv.axis0.config.enable_watchdog = False
 
 
-# ramp up
-for setpoint in range(0, 50):
-    print(f"Setting setpoint to {setpoint}")
-    drv.axis0.controller.input_vel = setpoint
-    sleep(0.1)
+def is_error(drv):
+    """check if there are any errors"""
+    ax = drv.axis0
 
-sleep(5)
+    for var in [
+        ax.error,
+        ax.motor.error,
+        ax.encoder.error,
+        ax.controller.error,
+        ax.sensorless_estimator.error,
+    ]:
+        if var > 0:
+            return True
 
-# ramp down
-for setpoint in range(50, 0, -1):
-    print(f"Setting setpoint to {setpoint}")
-    drv.axis0.controller.input_vel = setpoint
-    sleep(0.1)
+    return False
 
-drv.axis0.requested_state = enums.AxisState.IDLE
+
+# check for errors
+if is_error(drv):
+    print("ERROR found, exiting")
+    exit(1)
+
+# set positiion to zero
+drv.axis0.encoder.set_linear_count(0)
+
+
+setpoints = list(range(0, 50)) + 20 * [50] + list(range(50, 0, -1))
+
+
+def follow_curve(setpoints, delay=0.1):
+    """follow curve consisting of velocity setpoints"""
+
+    for setpoint in setpoints:
+        print(f"Setting setpoint to {setpoint}")
+        drv.axis0.controller.input_vel = setpoint
+        sleep(delay)
+        # print current velocity
+        print(f"Current velocity: {drv.axis0.encoder.vel_estimate}")
+        # print current position
+        print(f"Current position: {drv.axis0.encoder.pos_estimate}")
+
+
+follow_curve(setpoints)
