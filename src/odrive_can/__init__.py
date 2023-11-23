67e4f2cf63577337c5aea537921acfdd258f0180
Jev Kuznetsov <jev.kuznetsov@gmail.com>
2023-11-23 09:01:04 +0000

    add ids

--- src/odrive_can/__init__.py
+++ src/odrive_can/__init__.py
@@ -25,3 +25,10 @@ def get_dbc(name: str = "odrive-cansimple-0.5.6"):
 def get_axis_id(msg: can.Message) -> int:
     """get axis id from message"""
     return msg.arbitration_id >> 5
+
+
+def extract_ids(can_id: int) -> tuple[int, int]:
+    """get axis_id and cmd_id from can_id"""
+    cmd_id = can_id & 0x1F  # Extract lower 5 bits for cmd_id
+    axis_id = can_id >> 5  # Shift right by 5 bits to get axis_id
+    return axis_id, cmd_id
